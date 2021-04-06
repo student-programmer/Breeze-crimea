1057190ca40999e548ac0aa8bf9fab801d6f9b67
Roman Podobny <tazer16179@gmail.com>
2021-04-06 21:03:23 +0300

    i add works and catalog database

--- src/Redux/Actions/airs.js
+++ src/Redux/Actions/airs.js
@@ -7,9 +7,31 @@ import axios from 'axios'
     
  const fetchAirs = (sortBy, category) => (dispatch) =>{
     dispatch(setLoaded(false));
-    axios.get(`/conditioning?${category !== null ? `category=${category}` : ''}`).then(({data}) => {
+    axios.get(`http://localhost:5000/air/gets ${category && `/${category}` || '' }`).then(({data}) => {
         dispatch(setAirs(data));
-})};
+})
+// const sqlSelect = "SELECT * FROM breeze.air";
+// axios({
+//     method:'GET',
+//     url:'air/get',
+//     baseURL:'http:localhost:5000/',}).then(data => {
+//         dispatch(setAirs(data));
+//     }).catch(error => {
+//     console.log(error);
+//     });
+};
+
+// fetch('http://localhost:5000/air/get', {
+//       method: 'GET',
+//       headers: {
+//         'Content-Type': 'application/json',
+//       },
+//       body: JSON.stringify(),
+//     })
+//       .then((response) => response.json())
+//       .then((result) => alert(result.response.msg))
+//   };
+
 
 
 export const setAirs = (items) => ({
