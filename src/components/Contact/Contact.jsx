9c8d86a6e20a3546ab7350a222cf0abad297779a
Roman Podobny <tazer16179@gmail.com>
2021-05-31 21:54:51 +0300

    i am finishing contact component in my project

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,30 +1,38 @@
 import React from 'react';
 import ElementContact from './ElementContact';
+import cont from "./Contact.module.css"
 const Contact = () => {
   return (
-    <div className="wrapper">
+    <div className={cont.wrapper}>
+      <div className={cont.contact}>
+        <p className={cont.contactItem}>Контакты</p>
+        <span className={cont.line1}></span>
+        <span className={cont.line2}></span>
+      </div>
+      <div>
+        <h1 className={cont.consult}>
+          Мы предостовляем бесплатную консультацию по всем поросам, если вы
+          позвоните по номеру телефона.
+        </h1>
+      </div>
       <ElementContact
         contacts={[
           '+79788281091',
           '+79782725124',
-          'email - breezecrimea@gmail.com',
+          
         ]}
       />
       <div>
+      <p className={cont.weOnMap}>Мы на карте</p>
         <iframe
+        className={cont.map}
           src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2825.08378820745!2d34.08956661580682!3d44.92163157750234!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40eade90f2162891%3A0xe53b7bfb3d312c30!2z0JPRgNCw0LbQtNCw0L3RgdC60LDRjyDRg9C7LiwgMTA5LCDQodC40LzRhNC10YDQvtC_0L7Qu9GM!5e0!3m2!1sru!2s!4v1615639725709!5m2!1sru!2s"
-          width="600"
-          height="450"
+          width="1200"
+          height="700"
           style={{ marginRight: 0 + 'em' }}
           allowfullscreen=""
           loading="lazy"
         ></iframe>
-      </div>
-      <div>
-        <h1>
-          Мы предостовляем бесплатную консультацию по всем поросам, если вы
-          позвоните по номеру телефона.
-        </h1>
       </div>
     </div>
   );
--- src/components/Contact/ElementContact.jsx
+++ src/components/Contact/ElementContact.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
-
+import cont from "./Contact.module.css"
 const ElementContact = ({contacts}) => {
     return (
-        <div>
+        <div className={cont.element}>
             <ul>
-                 <p>Если вы хотите с намаи связаться позвоните по номеру -  </p> 
-                {
-                    contacts.map((contact, index)=> <li key={`${contact}_${index}`}>{contact}</li>)
+             
+            <p className={cont.inf1}>телефон:</p> 
+            <p className={cont.inf2}>email:</p>
+            <p className={cont.emailInf}>breezecrimea@gmail.com</p>
+            
+              {
+                    contacts.map((contact, index)=> <li className={cont.phoneNumber}key={`${contact}_${index}`}>{contact}</li>)
                 }
             </ul>
         </div>
